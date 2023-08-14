44c0dc8c2115da039cc84e9efe82bf6f46b5b5f8
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-08-14 13:37:41 +0300

    Add project files.

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Client App";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine($"Sending Message #{i+1}");
+    string message = $"Message #{i+1}";
+    byte[] body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchangeName, routingKey, null, body);
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+channel.Close();
+cnn.Close();
+
+Console.WriteLine("All messages sent!");
---
+++ RabbitServer1/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Server 01";
+
+IConnection cnn = factory.CreateConnection();
+
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += async (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    await Task.Delay(TimeSpan.FromSeconds(5));
+    Console.WriteLine($"Message received: {message}");
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadKey();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+cnn.Close();
